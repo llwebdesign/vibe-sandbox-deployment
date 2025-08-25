41c124500a48b050d167686b8f4cc10b80a534df
llwebdesign <lennert.loew@gmail.com>
2025-08-25 18:57:34 +0200

    Add src/lib/tetris/pieces.ts 1756141054193

---
+++ src/lib/tetris/pieces.ts
@@ -0,0 +1,102 @@
+export interface TetrominoShape {
+  shape: number[][];
+  color: string;
+}
+
+export interface Tetromino {
+  name: string;
+  rotations: number[][][];
+  color: string;
+}
+
+// Tetromino piece definitions with all 4 rotations
+export const TETROMINOES: Record<string, Tetromino> = {
+  I: {
+    name: 'I',
+    color: '#00f5ff',
+    rotations: [
+      [
+        [0, 0, 0, 0],
+        [1, 1, 1, 1],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+      ],
+      [
+        [0, 0, 1, 0],
+        [0, 0, 1, 0],
+        [0, 0, 1, 0],
+        [0, 0, 1, 0]
+      ],
+      [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [1, 1, 1, 1],
+        [0, 0, 0, 0]
+      ],
+      [
+        [0, 1, 0, 0],
+        [0, 1, 0, 0],
+        [0, 1, 0, 0],
+        [0, 1, 0, 0]
+      ]
+    ]
+  },
+  O: {
+    name: 'O',
+    color: '#ffff00',
+    rotations: [
+      [
+        [0, 1, 1, 0],
+        [0, 1, 1, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+      ],
+      [
+        [0, 1, 1, 0],
+        [0, 1, 1, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+      ],
+      [
+        [0, 1, 1, 0],
+        [0, 1, 1, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+      ],
+      [
+        [0, 1, 1, 0],
+        [0, 1, 1, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+      ]
+    ]
+  },
+  T: {
+    name: 'T',
+    color: '#a000f0',
+    rotations: [
+      [
+        [0, 1, 0],
+        [1, 1, 1],
+        [0, 0, 0]
+      ],
+      [
+        [0, 1, 0],
+        [0, 1, 1],
+        [0, 1, 0]
+      ],
+      [
+        [0, 0, 0],
+        [1, 1, 1],
+        [0, 1, 0]
+      ],
+      [
+        [0, 1, 0],
+        [1, 1, 0],
+        [0, 1, 0]
+      ]
+    ]
+  }
+};
+
+export const PIECE_NAMES = Object.keys(TETROMINOES);
